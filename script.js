973ea0c1f353e85ca99c817062cd843c1d2b98ab
shota <55336324+bet-lb2@users.noreply.github.com>
2025-02-10 17:26:07 +0900

    add three variables and operator function

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ const divide = (a, b) => {
     }
     return a / b;
 }
+
+let firstNum;
+let operator;
+let secondNum;
+
+const operate = (operator, numbers) => {
+    if (operator === "+") {
+        return add();
+    } else if (operator === "-") {
+        return subtract()
+    } else if (operator === "*") {
+        return multiply();
+    } else if (operator === "/") {
+        return divide();
+    }
+}

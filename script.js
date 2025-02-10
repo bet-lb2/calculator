883ba35483cc0ed7bb9015a65eac848fad24911d
shota <55336324+bet-lb2@users.noreply.github.com>
2025-02-10 17:19:57 +0900

    add some functions(add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    if (b === 0) {
+        return "Can't divide 0";
+    }
+    return a / b;
+}

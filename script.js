3f9364e4dadb3bd19f185ab7feff2ae3eabc38af
shota <55336324+bet-lb2@users.noreply.github.com>
2025-02-11 13:10:09 +0900

    html and css setting have almost finished, then js made a little progress
    js event listeners were implemented partially

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("#buttons button")
+const allClear = document.getElementById("all-clear");
+const invert = document.getElementById("invert");
+const toPercent = document.getElementById("to-percent");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+// console.log(buttons)
+
 const add = (a, b) => {
     return a + b;
 }
@@ -21,14 +31,39 @@ let firstNum;
 let operator;
 let secondNum;
 
-const operate = (operator, numbers) => {
+const operate = (operator, num1, num2) => {
     if (operator === "+") {
-        return add();
+        return add(num1, num2);
     } else if (operator === "-") {
-        return subtract()
+        return subtract(num1, num2)
     } else if (operator === "*") {
-        return multiply();
+        return multiply(num1, num2);
     } else if (operator === "/") {
-        return divide();
+        return divide(num1, num2);
     }
 }
+
+buttons.forEach(button => {
+    button.addEventListener("mousedown", (e) => {
+        e.target.style.opacity = "0.5";
+    })
+    button.addEventListener("mouseup", (e) => {
+        e.target.style.opacity = "1";
+    })
+})
+
+allClear.addEventListener("click", () => {
+    display.innerText = "";
+})
+
+numbers.forEach(number => {
+    number.addEventListener("click", (e) => {
+        display.innerText += e.target.innerText;
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        firstNum = display.innerText;
+    })
+})
